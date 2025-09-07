546dafb1ccf3e2ac56cdec2a3749e24f8a6a2224
Jabez <jabez.njue@strathmore.edu>
2025-09-07 23:07:52 +0300

    created log in & out

--- Forms/forms.php
+++ Forms/forms.php
@@ -1,5 +1,9 @@
 <?php
 class forms{
+    private function submit_button($value){
+    echo "<input type='submit' value='{$value}'>";
+    }
+
     public function signup(){
         ?>
         <h2>Signup Form BBIT</h2>
@@ -10,8 +14,21 @@ class forms{
             <input type='email' id='email' name='email' required><br>
             <label for='password'>Password:</label>
             <input type='password' id='password' name='password' required><br>
-            <input type='submit' value='Sign Up'>
+            <?php $this->submit_button('Sign Up'); ?> <a href="login.php">Already have an account? Login
         </form>
         <?php
+    }
+
+public function login(){
+    ?>
+    <h2>Login Form</h2>
+    <form action='submit_login.php' method='post'>
+        <label for='username'>Username:</label>
+        <input type='text' id='username' name='username' required><br>
+        <label for='password'>Password:</label>
+        <input type='password' id='password' name='password' required><br>
+        <?php $this->submit_button('Log In'); ?> <a href="index.php">Already have an account? Sign Up
+    </form>
+    <?php
     }
 }
