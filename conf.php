2f7b300f34f34f8e91d982d3c3b8778c38b3d8cc
Jabez <jabez.njue@strathmore.edu>
2025-09-07 22:51:49 +0300

    introduced Class Auto Load

---
+++ conf.php
@@ -0,0 +1,15 @@
+<?php
+// Site configuration
+$conf['site_name'] = "BBIT DevOps";
+$conf['admin_email'] = "info@bbit.edu";
+$conf['site_url'] = "http://localhost/bbit_API_grpd_2025/";
+
+// Site language
+$conf['language'] = "en";
+
+// Database constants
+$conf['db_type'] = "pdo";
+$conf['db_host'] = "localhost";
+$conf['db_user'] = "root";
+$conf['db_pass'] = "";
+$conf['db_name'] = "proj";
